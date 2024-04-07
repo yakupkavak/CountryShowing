97529909ce875c31ca50990c8895e66d89ab43a4
Yakup Kavak <yakupkavk@gmail.com>
2024-04-07 18:32:20 +0300

    Sistem ui created and implementations finished.

---
+++ app/src/main/java/com/example/countrynew/model/Model.kt
@@ -0,0 +1,10 @@
+package com.example.countrynew.model
+
+data class Country(
+    val countryName: String?,
+    val countryRegion: String?,
+    val countryCapital: String?,
+    val countryCurrency: String?,
+    val countryLanguage: String?,
+    val countryFlagUrl: String?,
+    )
