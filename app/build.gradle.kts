0d4d1d44b77c7ec0f00c35d7c8590f76e817e836
Yakup Kavak <yakupkavk@gmail.com>
2024-04-12 12:19:35 +0300

    updated implementation now it works.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
     }
     kapt {
         correctErrorTypes = true
-        includeCompileClasspath = false
+        includeCompileClasspath = true // Bu ayarı `true` yap
     }
 
 
@@ -44,6 +44,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    configurations.all {
+        resolutionStrategy {
+            force("org.jetbrains:annotations:23.0.0") // Bu sürüm projen için uygunsa
+        }
+    }
 }
 
 dependencies {
@@ -52,13 +57,11 @@ dependencies {
     val retrofitVersion = "2.3.0"
     val glideVersion = "4.9.0"
     val rxJavaVersion = "2.1.1"
-    val roomVersion = "2.2.3"
+    val room_version = "2.6.1"
     val navVersion = "2.2.1"
     val preferencesVersion = "1.1.0"
-    val room_version = "2.6.1"
 
     implementation("androidx.core:core-ktx:1.12.0")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -67,12 +70,31 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.guava:guava:30.1-jre")
     //implementation("android.arch.lifecycle:extensions:$lifeCycleExtensionVersion")
-    annotationProcessor("android.arch.persistence.room:compiler:1.1.1")
 
-    implementation("androidx.room:room-runtime:$roomVersion")
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin Symbol Processing (KSP) 
+    // ksp("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    //kapt("androidx.room:room-compiler:$roomVersion")
+    //implementation("androidx.room:room:$roomVersion")
+    //implementation("androidx.room:room-compiler:$roomVersion")
+    //implementation("androidx.room:room-ktx:$roomVersion")
+    //kapt("android.arch.persistence.room:compiler:1.1.1")
+
+
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0")
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
